3baffa26234ffa791f637d31f71ae55ce4dc73ef
Stanley Huang <huang.stanley050@gmail.com>
2020-02-10 14:20:00 +1100

    able to get the html file into string for saving to model HTML from buffer

--- controllers/data.js
+++ controllers/data.js
@@ -1,5 +1,6 @@
+const { parse } = require("node-html-parser");
 exports.upload = (req, res) => {
   console.log("this is upload route");
-  console.log(req.file);
+  console.log(req.file.buffer.toString("utf8"));
   res.send("hello");
 };
