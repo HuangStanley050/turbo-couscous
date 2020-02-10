e306aadf46e5a8bf266ac56b244aad1975f13412
Stanley Huang <huang.stanley050@gmail.com>
2020-02-10 15:15:45 +1100

    made readFile into promise

--- controllers/data.js
+++ controllers/data.js
@@ -1,15 +1,26 @@
 const fs = require("fs");
 const pdf = require("html-pdf");
 
-exports.upload = (req, res) => {
-  const html = fs.readFileSync(`./temp/${req.file.originalname}`, "utf8");
+exports.upload = async (req, res) => {
+  let html;
+  try {
+    html = await fs.promises.readFile(
+      `./temp/${req.file.originalname}`,
+
+      "utf8"
+    );
+  } catch (err) {
+    console.log(err);
+  }
   const options = { format: "Letter" };
+
   pdf
     .create(html, options)
     .toFile(`./temp/${req.file.originalname}.pdf`, function(err, res) {
       if (err) return console.log(err);
-      console.log(res); // { filename: '/app/businesscard.pdf' }
+      //console.log(res); // { filename: '/app/businesscard.pdf' }
     });
+
   console.log("this is upload route");
 
   res.send("hello");
