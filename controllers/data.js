b51b72aad050a4b5126035a8352f406bf0b67919
Stanley Huang <huang.stanley050@gmail.com>
2020-02-11 12:06:31 +1100

    timestamp works with file uploaded to google

--- controllers/data.js
+++ controllers/data.js
@@ -5,6 +5,7 @@ exports.upload = async (req, res) => {
   let html;
   const options = { format: "Letter" };
   const bucket = req.app.get("bucket");
+
   const fileName =
     req.file.originalname.substr(0, req.file.originalname.lastIndexOf(".")) ||
     req.file.originalname;
@@ -17,18 +18,20 @@ exports.upload = async (req, res) => {
   } catch (err) {
     console.log(err);
   }
-
+  let timeStamp = Date.now();
   pdf
     .create(html, options)
-    .toFile(`./temp/${fileName}-${Date.now()}.pdf`, function(err, res) {
+    .toFile(`./temp/${fileName}-${timeStamp}.pdf`, err => {
       if (err) return console.log(err);
       //console.log(res); // { filename: '/app/businesscard.pdf' }
+      const bucketFileName = `${fileName}-${timeStamp}.pdf`;
+      const file = bucket.file(bucketFileName);
+      fs.createReadStream(`./temp/${bucketFileName}`)
+        .pipe(file.createWriteStream())
+        .on("error", err => console.log(err))
+        .on("finish", () => {
+          console.log("finished");
+          return res.send("Upload completed");
+        });
     });
-
-  const file = bucket.file("test.html");
-  fs.createReadStream("./temp/test.html")
-    .pipe(file.createWriteStream())
-    .on("error", err => console.log(err))
-    .on("finish", () => console.log("finished"));
-  res.send("hello");
 };
